9eb4f8eddf960d7b747330db573963050c34d0c0
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-08-07 20:27:15 +0530

    import java.util.Scanner; public class takeInputPrint{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String temp=sc.nextLine();         System.out.printf(temp);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class takeInputPrint{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String temp=sc.nextLine();
+        System.out.printf(temp);
+    }
+}
