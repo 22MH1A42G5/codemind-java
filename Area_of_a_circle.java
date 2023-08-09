de959eb0aa4ebbcf308b96a8740e6979578327bc
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-08-09 22:21:46 +0530

    import java.util.Scanner; public class areaOfCircle{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         System.out.printf("%.2f",r*r*3.14);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class areaOfCircle{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        System.out.printf("%.2f",r*r*3.14);
+    }
+}
