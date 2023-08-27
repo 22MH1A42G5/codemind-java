fcf1c931b36f346171a2e45353e4c1ae60356fbf
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-08-27 13:09:12 +0530

    import java.util.Scanner; public class RainDrop{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%3==0 || n%5==0 || n%7==0){             if(n%3==0)             System.out.print("Pling");             if(n%5==0)             System.out.print("Plang");             if(n%7==0)             System.out.print("Plong");         }         else{             System.out.print(n);         }     } }

---
+++ RainDrop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RainDrop{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%3==0 || n%5==0 || n%7==0){
+            if(n%3==0)
+            System.out.print("Pling");
+            if(n%5==0)
+            System.out.print("Plang");
+            if(n%7==0)
+            System.out.print("Plong");
+        }
+        else{
+            System.out.print(n);
+        }
+    }
+}
