5cee450a72988542182801d580a694acc16b5b94
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-08-27 18:03:22 +0530

    import java.util.Scanner; public class nextpal{     static boolean prime(int n) {         for (int i = 2; i <=(n/2)+1; i++) {             if (n % i == 0) {                 return false;             }         }         return true;     }     static boolean palind(int n){         int t=n,rem,rev=0;         while(t!=0){             rem=t%10;             rev=rev*10+rem;             t/=10;         }         if(rev==n)         return true;         else         return false;     }     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i=n+1;         while(true){             if(prime(i)&& palind(i)){                 System.out.println(i);                 break;             }             i+=1;         }     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
-public class palind{
-    static boolean palindrome(int n){
+public class nextpal{
+    static boolean prime(int n) {
+        for (int i = 2; i <=(n/2)+1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    static boolean palind(int n){
         int t=n,rem,rev=0;
         while(t!=0){
             rem=t%10;
             rev=rev*10+rem;
             t/=10;
         }
-        if(n==rev){
-            return true;
-        }
-        else 
+        if(rev==n)
+        return true;
+        else
         return false;
     }
-    static boolean prime(int n){
-        for(int i=2;i<=(n/2)+1;i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int i=n+1;
         while(true){
-            if(palindrome(i) && prime(i)){
+            if(prime(i)&& palind(i)){
                 System.out.println(i);
                 break;
             }
