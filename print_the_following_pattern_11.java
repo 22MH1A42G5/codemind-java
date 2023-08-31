8fd1215f5b539d8f7add8439c50a7f43c36533f3
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-08-31 11:18:27 +0530

    import java.util.Scanner; public class patt{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=65;i<n+65;i++){             for(int j=0;j<n;j++){                 System.out.print((char)i+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern_11.java
+++ print_the_following_pattern_11.java
@@ -3,9 +3,9 @@ public class patt{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=65;i<65+n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.printf("%c ",i);
+        for(int i=65;i<n+65;i++){
+            for(int j=0;j<n;j++){
+                System.out.print((char)i+" ");
             }
             System.out.println();
         }
