2c36ec7c78f033857e4880ffc290b9b9035201a6
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-09-09 17:26:38 +0530

    import java.util.Scanner;
    public class codemind2 {
        static int prime(int n) {
            if (n == 1 || n == 0)
                return 0;
            for (int i = 2; i <=(n + 1) / 2; i++) {
                if (n % i == 0)
                    return 0;
            }
            return 1;
        }
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            int temp=n,rem;
            if (prime(n)==1){
                while(temp!=0){
                    rem=temp%10;
                    temp/=10;
                    if(prime(rem)==1){
                        
                    }
                    else{
                        System.out.println("Not Mega Prime");
                        return;
                    }
                }
                System.out.println("Mega Prime");
            }
            else{
                System.out.println("Not Mega Prime");
            }
        }
    }

---
+++ mega_prime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class codemind2 {
+    static int prime(int n) {
+        if (n == 1 || n == 0)
+            return 0;
+        for (int i = 2; i <=(n + 1) / 2; i++) {
+            if (n % i == 0)
+                return 0;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp=n,rem;
+        if (prime(n)==1){
+            while(temp!=0){
+                rem=temp%10;
+                temp/=10;
+                if(prime(rem)==1){
+                    
+                }
+                else{
+                    System.out.println("Not Mega Prime");
+                    return;
+                }
+            }
+            System.out.println("Mega Prime");
+        }
+        else{
+            System.out.println("Not Mega Prime");
+        }
+    }
+}
