1800ce7d44246c0abf19f77572a5284251a1ede2
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-09-14 22:37:00 +0530

    import java.util.*; public class Elements{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         ArrayList<Integer> arr=new ArrayList<Integer>();         ArrayList<Integer> odd=new ArrayList<Integer>();         ArrayList<Integer> eve=new ArrayList<Integer>();         for(int i=0;i<n;i++){             arr.add(sc.nextInt());         }         for(int i=0;i<n;i++){             if(arr.get(i)%2==0){                 eve.add(arr.get(i));             }             else             odd.add(arr.get(i));         }         for(int i=0;i<eve.size();i++){             System.out.print(eve.get(i)+" ");         }         for(int i=0;i<odd.size();i++){             System.out.print(odd.get(i)+" ");         }     } }

---
+++ Even_elements_followed_by_Odd_elements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Elements{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        ArrayList<Integer> odd=new ArrayList<Integer>();
+        ArrayList<Integer> eve=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            arr.add(sc.nextInt());
+        }
+        for(int i=0;i<n;i++){
+            if(arr.get(i)%2==0){
+                eve.add(arr.get(i));
+            }
+            else
+            odd.add(arr.get(i));
+        }
+        for(int i=0;i<eve.size();i++){
+            System.out.print(eve.get(i)+" ");
+        }
+        for(int i=0;i<odd.size();i++){
+            System.out.print(odd.get(i)+" ");
+        }
+    }
+}
