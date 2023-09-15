dd5f368557eadc9eaae9f558d0213e50e7f0fd67
K.SRI RAM <119724502+22MH1A42G5@users.noreply.github.com>
2023-09-15 21:41:22 +0530

    import java.util.*; public class avg{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int sum=0;         int avg=0;         int c=0;         ArrayList<Integer> arr= new ArrayList<Integer>();         for(int i=0;i<n;i++){             arr.add(sc.nextInt());         }         for(int i=0;i<n;i++){             sum+=arr.get(i);         }         avg=sum/n;         for(int i:arr){             if(i<=avg)             c++;         }         System.out.println(c);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class avg{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        int avg=0;
+        int c=0;
+        ArrayList<Integer> arr= new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            arr.add(sc.nextInt());
+        }
+        for(int i=0;i<n;i++){
+            sum+=arr.get(i);
+        }
+        avg=sum/n;
+        for(int i:arr){
+            if(i<=avg)
+            c++;
+        }
+        System.out.println(c);
+    }
+}
